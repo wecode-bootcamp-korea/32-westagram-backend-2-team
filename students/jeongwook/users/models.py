7448a25e6948ecb48253161ac9c1e5d00ba14ab7
ma <ma@maui-MacBook-Pro.local>
2022-04-14 16:38:43 +0900

    Add : user model 2nd

--- students/jeongwook/users/models.py
+++ students/jeongwook/users/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 # Create your models here.
 
-class Uesr(models.Model) :
-    name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=150)
-    password = models.CharField(max_length=50)
-    phone_number = models.CharField(max_length=12)
+class User(models.Model) :
+    name         = models.CharField(max_length=50)
+    email        = models.EmailField(max_length=150, required=True)
+    password     = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=30)
+    created_at   = models.DateTimeField(auto_now_add=True)
+    updated_at   = models.DateTimeField(auto_now=True)
 
     class Meta :
         db_table = 'users'
