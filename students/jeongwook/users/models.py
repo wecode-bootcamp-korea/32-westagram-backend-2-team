da0b91c428df3e85335913b1b564f87ed15b23fd
ma <ma@maui-MacBook-Pro.local>
2022-04-14 17:15:43 +0900

    Add : user model 3rd

--- students/jeongwook/users/models.py
+++ students/jeongwook/users/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class User(models.Model) :
     name         = models.CharField(max_length=50)
-    email        = models.EmailField(max_length=150, required=True)
+    email        = models.EmailField(max_length=150, unique=True)
     password     = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=30)
     created_at   = models.DateTimeField(auto_now_add=True)
