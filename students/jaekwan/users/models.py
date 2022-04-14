8c8c8e2bc0579a155a23288ee9a85c826cb2de67
BDjaekwanee <jaekwanee@gmail.com>
2022-04-14 16:15:06 +0900

    Add: Create users model

--- students/jaekwan/users/models.py
+++ students/jaekwan/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=45)
+    email = models.CharField(max_length=45)
+    password = models.CharField(max_length=45)
+    mobile_number = models.CharField(max_length=16)
+
+    class Meta:
+        db_table = 'users'
