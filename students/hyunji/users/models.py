1e604f616dc41f729978acf6d84eaff7ae287da7
celeste-6 <sage.celeste6@gmail.com>
2022-04-14 19:20:05 +0900

    Add: Create users model

--- students/hyunji/users/models.py
+++ students/hyunji/users/models.py
@@ -4,7 +4,9 @@ class User(models.Model):
     name = models.CharField(max_length=30)
     email = models.CharField(max_length=50, unique=True)
     password = models.CharField(max_length=200)
-    mobile_number = models.CharField(max_length=25, unique=True)
+    mobile_number = models.CharField(max_length=25)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'users'
