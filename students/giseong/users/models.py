67dc14cf862330d513aa9500ea877511886be12f
jiggy <jiggyjiggy0323@gmail.com>
2022-04-15 09:11:27 +0900

    Style: Fix variable name

--- students/giseong/users/models.py
+++ students/giseong/users/models.py
@@ -1,12 +1,11 @@
 from django.db import models
-from django.core.validators import RegexValidator
 
 # Create your models here.
 class User(models.Model):
-    user_name         = models.CharField(max_length=20)
-    user_email        = models.EmailField(max_length=128, null=False, unique=True)
-    user_password     = models.CharField(max_length=128, null=False)
-    user_phone_number = models.CharField(max_length=12, unique=True)
+    name         = models.CharField(max_length=20)
+    email        = models.EmailField(max_length=128, null=True, unique=True)
+    password     = models.CharField(max_length=128, null=True)
+    phone_number = models.CharField(max_length=12, unique=True)
 
     class Meta:
         db_table = 'users'
