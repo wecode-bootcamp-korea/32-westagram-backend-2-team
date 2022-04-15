95411fd7930598fe4b71fc2ff43d4d046dceb0d1
jiggy <jiggyjiggy0323@gmail.com>
2022-04-15 13:30:16 +0900

    Refactor: Add attribute created_at, updated_at for history management

--- students/giseong/users/models.py
+++ students/giseong/users/models.py
@@ -3,9 +3,11 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     name         = models.CharField(max_length=20)
-    email        = models.EmailField(max_length=128, null=True, unique=True)
-    password     = models.CharField(max_length=128, null=True)
-    phone_number = models.CharField(max_length=12, unique=True)
+    email        = models.EmailField(max_length=128, unique=True)
+    password     = models.CharField(max_length=128)
+    phone_number = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'users'
