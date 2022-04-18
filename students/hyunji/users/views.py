6f6751e75d5ea99ba280c5220621448f4f732f83
celeste-6 <sage.celeste6@gmail.com>
2022-04-18 15:17:16 +0900

    Feat: Add Signup View

--- students/hyunji/users/views.py
+++ students/hyunji/users/views.py
@@ -9,26 +9,20 @@ class SignUpView(View):
         data = json.loads(request.body)
 
         try:
-            # name = data['name']
             email = data['email']
             password = data['password']
             mobile_number = data['mobile_number']
 
-            # 정규표현식
-            REGEX_EMAIL = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
-            REGEX_PASSWORD = "^.*(?=^.{8,}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%*^&+=]).*$"
-            REGEX_MOBILE_NUMBER = '\d{3}-\d{3,4}-\d{4}'
+            REGEX_EMAIL =         '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+            REGEX_PASSWORD =      "^.*(?=^.{8,}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%*^&+=]).*$"
+            REGEX_MOBILE_NUMBER = '^([+]\d{2})?\d{10}$'
 
-            # Email Validation
             if not re.match(REGEX_EMAIL, email):
                 return JsonResponse({"message": "INVALID_EMAIL"}, status=400)
-            # Password Validation
             if not re.match(REGEX_PASSWORD, password):
                 return JsonResponse({"message": "INVALID_PASSWORD"}, status=400)
-            # Mobile Number Validation
             if not re.match(REGEX_MOBILE_NUMBER, mobile_number):
                 return JsonResponse({"message": "INVALID_PHONE_NUMBER"}, status=400)
-            # Email Integrity
             if User.objects.filter(email=email).exists():
                 return JsonResponse({"message": "EMAIL_ALREADY_EXISTS"}, status=400)
 
