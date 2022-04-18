8610ddf3d323ec72876413f6a9e5a284f5b6662c
ma <ma@maui-MacBook-Pro.local>
2022-04-18 16:45:30 +0900

    email error message change and Modify variables in constant data to uppercase

--- students/jeongwook/users/validation.py
+++ students/jeongwook/users/validation.py
@@ -5,16 +5,16 @@ from django.core.exceptions import ValidationError
 from django.http  import JsonResponse
 
 def email_validate(email):
-    regexr_email = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
-    if re.match(regexr_email, email) == None:
+    REGEXR_EMAIL = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+    if re.match(REGEXR_EMAIL, email) == None:
         raise ValidationError("INVALID_EMAIL")
 
 def password_validate(password):
-    regexr_password = '^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!%*#?&])[A-Za-z0-9$@$!%*#?&].{8,}$'
-    if re.match(regexr_password, password) == None:
+    REGEXR_PASSWORD = '^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!%*#?&])[A-Za-z0-9$@$!%*#?&].{8,}$'
+    if re.match(REGEXR_PASSWORD, password) == None:
         raise ValidationError ("ERROR_PASSWORD_STATUS")
 
 def phonenumber_validate(phone_number):
-    regexr_phonenumber = '^\s*(?:\+?(\d{1,3}))?([-. (]*(\d{3})[-. )]*)?((\d{3})[-. ]*(\d{2,4})(?:[-.x ]*(\d+))?)\s*$'
-    if re.match(regexr_phonenumber, phone_number) == None:
+    REGEXR_PHONENUMBER = '^\s*(?:\+?(\d{1,3}))?([-. (]*(\d{3})[-. )]*)?((\d{3})[-. ]*(\d{2,4})(?:[-.x ]*(\d+))?)\s*$'
+    if re.match(REGEXR_PHONENUMBER, phone_number) == None:
         raise ValidationError ("PHONENUMBER_ERROR")
