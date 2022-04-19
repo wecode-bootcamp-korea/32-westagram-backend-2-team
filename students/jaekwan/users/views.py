692c6d61c69a7395c6295964982bfc122db0e6d6
BDjaekwanee <jaekwanee@gmail.com>
2022-04-19 15:53:17 +0900

    Add: Create LogInView & Regular Expression Modularization

--- students/jaekwan/users/views.py
+++ students/jaekwan/users/views.py
@@ -2,30 +2,23 @@ import json
 import re
 from django.http  import JsonResponse
 from django.views import View
-
 from users.models import User
-
-EMAIL_REGEX          = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
-PASSWORD_REGEX       = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
-MOBILE_NUMBER_REGEX  = '^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$'
-
-
+from .validation   import SignUpValidation
 class SignupView(View):
     def post(self, request):
         try:
+
             data          = json.loads(request.body)
             email         = data['email']
             password      = data['password']
             mobile_number = data['mobile_number']
 
-            if not re.match(EMAIL_REGEX, email):
-                return JsonResponse({'MESSAGE':'EMAIL_ERROR'}, status=400)
+            SignUpValidation.validate_email(email)
+            SignUpValidation.validate_password(password)
+            SignUpValidation.validate_mobile_number(mobile_number)
 
-            if not re.match(PASSWORD_REGEX, password):
-                return JsonResponse({'MESSAGE':'PASSWORD_ERROR'}, status=400)
-            
-            if not re.match(MOBILE_NUMBER_REGEX, mobile_number):
-                return JsonResponse({'MESSAGE':'MOBILE_NUMBER_ERROR'}, status=400)
+            if User.objects.filter(email=email).exists():
+                return JsonResponse({"message": "EMAIL_EXISTS_ERROR"}, status=400)
             
             User.objects.create(
                 name            = data['name'],
@@ -36,3 +29,18 @@ class SignupView(View):
             return JsonResponse({'MESSAGE':'SUCCESS'}, status=201)
         except KeyError:
             return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
+
+class LoginView(View):
+    def post(self, request):
+        try:
+            data           = json.loads(request.body)
+            email          = data['email']
+            password       = data['password']
+            if not email or not password:
+                return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)  
+            if User.objects.filter(email = email, password = password).exists():
+                return JsonResponse({'MESSAGE':'SUCCESS'}, status=200)
+            return JsonResponse({'MESSAGE':'INVALID_USER'}, status=401)
+            
+        except KeyError:
+            return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)        
